a0171e960c8c750ad580c002468eb95c55d978fb
shreyapradeep <137262361+shreyapradeep@users.noreply.github.com>
2024-03-23 22:02:05 +0530

    Update Sitemap_URL_Scrape.py
    The output of this code would give u all the links (.pdf,.docs,normal) from all the sitemaps in "https://www.cancer.gov/sitemap.xml"

--- ShreyaPradeep/Sitemap_URL_Scrape.py
+++ ShreyaPradeep/Sitemap_URL_Scrape.py
@@ -1,4 +1,36 @@
+#this is a more optmized and better code
 import requests
+from bs4 import BeautifulSoup
+
+def extract_urls_from_xml(urls):
+  normal_urls = set()
+  pdf_urls = set()
+  docx_urls = set()
+  for i in urls:
+    response = requests.get(i)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'xml')
+        for loc in soup.find_all('loc'):
+            url = loc.text.strip()
+            if url.lower().endswith('.pdf'):
+                pdf_urls.add(url)
+            elif url.lower().endswith('.docx'):
+                docx_urls.add(url)
+            else:
+                normal_urls.add(url)
+
+  return normal_urls, pdf_urls, docx_urls
+url = "https://www.cancer.gov/sitemap.xml"
+urls = []
+response = requests.get(url)
+if response.status_code == 200:
+  soup = BeautifulSoup(response.content, 'xml')
+  loc_tags = soup.find_all('loc')
+  urls = [loc.text for loc in loc_tags]
+extract_urls_from_xml(urls)
+
+
+'''import requests
 from bs4 import BeautifulSoup
 
 def extract_urls_from_xml(url):
@@ -38,4 +70,8 @@ for url in pdf_urls:
 print("\nDOCX URLs:")
 for url in docx_urls:
     print(url)
+    '''
+
+
+
 
