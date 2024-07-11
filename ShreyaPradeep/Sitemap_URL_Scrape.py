ce9990263afd901355ba0459a47af3e54afd9e84
shreyapradeep <137262361+shreyapradeep@users.noreply.github.com>
2024-07-11 10:53:43 +0530

    Update Sitemap_URL_Scrape.py

--- ShreyaPradeep/Sitemap_URL_Scrape.py
+++ ShreyaPradeep/Sitemap_URL_Scrape.py
@@ -1,77 +1,43 @@
-#this is a more optmized and better code
 import requests
 from bs4 import BeautifulSoup
 
-def extract_urls_from_xml(urls):
-  normal_urls = set()
-  pdf_urls = set()
-  docx_urls = set()
+def extract_urls_from_xml(urls,normal_urls,pdf_urls,docx_urls,pptx_urls, xlxs_urls,vid_urls):
   for i in urls:
     response = requests.get(i)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'xml')
         for loc in soup.find_all('loc'):
             url = loc.text.strip()
-            if url.lower().endswith('.pdf'):
+            if 'espanol' not in url:
+              if url.lower().endswith('.pdf'):
                 pdf_urls.add(url)
-            elif url.lower().endswith('.docx'):
+              elif url.lower().endswith('.docx'):
                 docx_urls.add(url)
-            else:
+              elif url.lower().endswith('.pptx'):
+                pptx_urls.add(url)
+              elif url.lower().endswith('.xlsx'):
+                xlxs_urls.add(url)
+              elif 'video' in url.lower() or url.lower().endswith(".mp4"):
+                vid_urls.add(url)
+              else:
                 normal_urls.add(url)
+  print(len(normal_urls))
+  #return normal_urls, pdf_urls, docx_urls
 
-  return normal_urls, pdf_urls, docx_urls
 url = "https://www.cancer.gov/sitemap.xml"
 urls = []
+normal_urls = set()
+pdf_urls = set()
+docx_urls = set()
+xlxs_urls=set()
+pptx_urls=set()
+vid_urls=set()
 response = requests.get(url)
 if response.status_code == 200:
   soup = BeautifulSoup(response.content, 'xml')
   loc_tags = soup.find_all('loc')
   urls = [loc.text for loc in loc_tags]
-extract_urls_from_xml(urls)
 
-
-'''import requests
-from bs4 import BeautifulSoup
-
-def extract_urls_from_xml(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'xml')  
-        normal_urls = set()
-        pdf_urls = set()
-        docx_urls = set()
-
-        for loc in soup.find_all('loc'):
-            url = loc.text.strip()
-            if url.lower().endswith('.pdf'):
-                pdf_urls.add(url)
-            elif url.lower().endswith('.docx'):
-                docx_urls.add(url)
-            else:
-                normal_urls.add(url)
-
-        return normal_urls, pdf_urls, docx_urls
-    else:
-        print("Failed to fetch the page:", response.status_code)
-        return set(), set(), set()
-
-#change sitemap url here
-url = "https://www.cancer.gov/nano/sitemap.xml"
-normal_urls, pdf_urls, docx_urls = extract_urls_from_xml(url)
-
-print("Normal URLs:")
-for url in normal_urls:
-    print(url)
-
-print("\nPDF URLs:")
-for url in pdf_urls:
-    print(url)
-
-print("\nDOCX URLs:")
-for url in docx_urls:
-    print(url)
-    '''
-
-
-
+print(len(urls))
+extract_urls_from_xml(urls,normal_urls,pdf_urls,docx_urls,pptx_urls, xlxs_urls,vid_urls)
 
